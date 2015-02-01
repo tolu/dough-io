057ccbfaa28dd8fdcc6090baa1df648bd0222573
Tobias Lundin <tobias.lundin@gmail.com>
2015-02-01 17:21:08 +0100

    added initial app.js

---
+++ src/scripts/app.js
@@ -0,0 +1,47 @@
+(function (Firebase) {
+  'use strict';
+
+  // SETUP
+
+  // 1. connect to firebase db
+
+  // 1.1. do we have a doughIO-id : ?{id}
+
+  // 1.1.1 yes => get that reference data and go to step 2
+
+  // 1.1.2 no  => allow user to create new doughIO-id
+
+  // 1.2.1 make sure doughIO-id is not taken
+
+  // 1.1.2 go to step 2
+
+  // LOAD doughIO
+
+  // 2. Get or create node for doughIO-id
+
+  // 2.1 Render node data
+
+  // 2.1.1 Display people in dough
+
+  // 2.1.2 Display costs in dough
+
+  // 2.2.1 Display form to add people
+
+  // 2.2.2 Display form to add cost
+
+  // INTERACT
+
+  // 3.1 Add person
+
+  // 3.1.1 Edit person
+
+  // 3.1.2 Remove person
+
+  // 3.2 Add cost
+  
+  // 3.2.1 Edit cost
+
+  // 3.2.2 Remove cost
+
+
+})(Firebase);
